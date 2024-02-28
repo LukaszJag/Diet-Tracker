6338227677935e45b19fbb0edc3f29aa67637e20
SubZero <lukasz.jagnow@gmail.com>
2024-02-28 19:16:19 +0100

    Add new method to SQLSelect class

--- src/sql_tools/SQLSelect.java
+++ src/sql_tools/SQLSelect.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class SQLSelect {
-    public static String getRowFromProductTableByProductName(String productName) throws SQLException {
+    public static String getRowFromProductTableByProductNameGetString(String productName) throws SQLException {
         ResultSet resultSet;
         Statement statement;
 
@@ -24,8 +24,34 @@ public class SQLSelect {
             for (int i = 0; i < Config.SQL_COLUMNS_FOR_INSERT_INTO_PRODUCT_TABLE.length; i++) {
                 result += resultSet.getString(Config.SQL_COLUMNS_FOR_INSERT_INTO_PRODUCT_TABLE[i].replace("`","")) + " ";
             }
-            System.out.println("Result of Query" + result + "\n");
         }
+        System.out.println("Query result: \n" + result);
+
+        return result;
+    }
+
+    public static String[] getRowFromProductTableByProductNameGetArray(String productName) throws SQLException {
+        ResultSet resultSet;
+        Statement statement;
+
+        String sql = "SELECT * FROM diet_tracker_schema.product_table WHERE product_name=\"" + productName + "\";";
+        String[] result = new String[Config.SQL_COLUMNS_CALENDAR.length];
+
+
+        Connection connection = GetConnection.getConnectionWithLocalHost();
+        statement = connection.createStatement();
+        resultSet = statement.executeQuery(sql);
+
+        while(resultSet.next()){
+            for (int i = 0; i < Config.SQL_COLUMNS_FOR_INSERT_INTO_PRODUCT_TABLE.length; i++) {
+                result[i] = resultSet.getString(Config.SQL_COLUMNS_FOR_INSERT_INTO_PRODUCT_TABLE[i].replace("`",""));
+            }
+        }
+        System.out.println("Query result: \n");
+        for (int i = 0; i < result.length; i++) {
+            System.out.println("[" + i + "]: " + result[i]);
+        }
+
         return result;
     }
 }
