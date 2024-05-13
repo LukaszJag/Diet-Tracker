4f751f3d9a0b2e19f1db9eab6dae43a8e3d7642e
SubZero <lukasz.jagnow@gmail.com>
2024-05-13 09:35:36 +0200

    Add new product to - 2024-05-12

--- src/runners_and_tests/Main.java
+++ src/runners_and_tests/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("\nRun Diet Tracker...");
         MainWindow mainWindow = new MainWindow();
-        GUIManager
+        //GUIManager
     }
 }
