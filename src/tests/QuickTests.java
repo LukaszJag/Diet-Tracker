94babcdb28661c201e6bee8a349f74021d96ea58
SubZero <lukasz.jagnow@gmail.com>
2024-05-15 11:33:03 +0200

    Start correcting day name in gui - calendar v 0.1

--- src/tests/QuickTests.java
+++ src/tests/QuickTests.java
@@ -12,7 +12,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class QuickTests {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, ParseException {
         //test1();
         //test2();
         //test3();
@@ -27,11 +27,12 @@ public class QuickTests {
         //test12();
         //test13();
         //test14();
-        test15();
+        //test15();
+        //test16();
     }
 
 
-
+    //<editor-fold desc="TESTS 1 -> 10">
     private static void test1() {
         String[] testArray = ImportDateFromTXTFilesToSQLDB.getPureDateFromFileInArray(FilesTools.convertFileToStringArray("src/data_store_and_backup/text_files/products/Burak.txt"));
         for (int i = 0; i < testArray.length; i++) {
@@ -39,7 +40,7 @@ public class QuickTests {
         }
     }
 
-    private static void test2(){
+    private static void test2() {
         String[] fileInArray = FilesTools.convertFileToStringArray("src/data_store_and_backup/text_files/products/Burak.txt");
         String readyToInsertQuery = ImportDateFromTXTFilesToSQLDB.convertTextFileToSQLQuery(fileInArray);
         try {
@@ -50,13 +51,13 @@ public class QuickTests {
     }
 
     private static void test3() {
-        DirectoryTools.makeDirectory("src/data_store_and_backup/text_files/days/","test1");
+        DirectoryTools.makeDirectory("src/data_store_and_backup/text_files/days/", "test1");
     }
 
     private static void test4() {
         if (DirectoryTools.doesDirectoryExist("src/data_store_and_backup/text_files/days/test2")) {
             System.out.println("Directory exist");
-        }else{
+        } else {
             System.out.println("Directory doesn't exist");
         }
     }
@@ -82,7 +83,7 @@ public class QuickTests {
     }
 
     // print all data from DayInCalendarObject
-    public static void test8(){
+    public static void test8() {
         DayInCalendar dayInCalendar = DayInCalendarFactoryToMakeTest.getExampleDayInCalendar();
         String[] dayInCalendarDataInArray = dayInCalendar.dayDataInStringArray(dayInCalendar);
 
@@ -90,15 +91,19 @@ public class QuickTests {
             System.out.println("i: [" + i + "]: " + dayInCalendarDataInArray[i]);
         }
     }
-    public static void test9(){
+
+    public static void test9() {
         DayInCalendar dayInCalendar = DayInCalendarFactoryToMakeTest.getExampleDayInCalendar();
         DayInCalendar.dayDataShowAllData(dayInCalendar);
 
     }
 
-    public static void test10(){
+    public static void test10() {
         InsertToCalendarDayTable.createInsertSQLQueryForCalendarDay(DayInCalendarFactoryToMakeTest.getExampleDayInCalendar());
     }
+    //</editor-fold>
+
+    //<editor-fold desc="TEST 11 -> 15">
     private static void test11() {
         try {
             SQLSelect.getAllProductNamesFromProductTable();
@@ -119,7 +124,6 @@ public class QuickTests {
         System.out.println(sqlStatement);
     }
 
-
     private static void test14() {
         String[] allUnique = SelectDistinctValues.selectAllUniqueCalendarByDayDate();
         for (int i = 0; i < allUnique.length; i++) {
@@ -130,4 +134,78 @@ public class QuickTests {
     private static void test15() {
         DirectoryTools.printAllFilesInAllDirectory("src/data_store_and_backup/text_files/days");
     }
+    //</editor-fold>
+
+    private static void test16() throws ParseException {
+
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", java.util.Locale.ENGLISH);
+
+            Date date = null;
+
+            date = sdf.parse("19/05/2024");
+
+            //specifies the pattern to print
+            sdf.applyPattern("EEEE");
+            String str = sdf.format(date);
+
+            //prints day name with date
+            System.out.println(str);
+
+            String dateString = "";
+            String[] dateStringArray = {"19/05/2024", "13/05/2024", "26/05/2024", "19-05-2024", "13-05-2024", "26-05-2024","2024-05-24",
+            "2024-04-08",
+            "2024-04-13",
+            "2024-04-26",
+            "2024-04-28",
+            "2024-05-02",
+            "2024-05-12"};
+
+
+        System.out.println(dateStringArray[7] + " GOOD");
+        System.out.println();
+        System.out.println(dateStringArray[8] + " GOOD");
+        System.out.println(dateStringArray[9] + " GOOD");
+        System.out.println(dateStringArray[10] + " GOOD");
+        System.out.println(dateStringArray[11] + " GOOD");
+        System.out.println(dateStringArray[12] + " GOOD" );
+        System.out.println();
+
+        for (int i = 0; i < dateStringArray.length; i++) {
+            if (i <= 2) {
+                sdf = new SimpleDateFormat("dd/MM/yyyy", java.util.Locale.ENGLISH);
+                date = sdf.parse(dateStringArray[i]);
+                sdf.applyPattern("EEEE");
+                dateString = sdf.format(date);
+                System.out.println(dateStringArray[i] + " -> " + dateString);
+            }
+            if (i > 2 && i <=5){
+                sdf = new SimpleDateFormat("dd-MM-yyyy", java.util.Locale.ENGLISH);
+                date = sdf.parse(dateStringArray[i]);
+                sdf.applyPattern("EEEE");
+                dateString = sdf.format(date);
+                System.out.println(dateStringArray[i] + " -> " + dateString);
+            }
+            if (i ==6){
+                sdf = new SimpleDateFormat("yyyy-MM-dd", java.util.Locale.ENGLISH);
+                date = sdf.parse(dateStringArray[i]);
+                sdf.applyPattern("EEEE");
+                dateString = sdf.format(date);
+                System.out.println(dateStringArray[i] + " -> " + dateString);
+
+                System.out.println();
+                System.out.println("DIRECT FROM SQL");
+            }
+            if(i > 6){
+                sdf = new SimpleDateFormat("yyyy-MM-dd", java.util.Locale.ENGLISH);
+                date = sdf.parse(dateStringArray[i]);
+                sdf.applyPattern("EEEE");
+                dateString = sdf.format(date);
+                System.out.println(dateStringArray[i] + " -> " + dateString);
+            }
+        }
+
+
+    }
+
+
 }
