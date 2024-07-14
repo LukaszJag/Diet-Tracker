4ae96004b49bb8c41240968f8087d0c26cdb9970
SubZero <lukasz.jagnow@gmail.com>
2024-07-14 22:10:00 +0200

    Add new test for SQLTests

--- src/runners_and_tests/tests/all_tests/SQLTests.java
+++ src/runners_and_tests/tests/all_tests/SQLTests.java
@@ -2,14 +2,17 @@ package runners_and_tests.tests.all_tests;
 
 import tools.sql_tools.CheckIfRowExist;
 import tools.sql_tools.SQLSelect;
+import tools.sql_tools.days_statistics.SelectFromDaysStatistics;
+import tools.sql_tools.general.GetResultSet;
 
 import java.sql.SQLException;
 
 public class SQLTests {
     public static void main(String[] args) throws SQLException {
-        test1();
-        test2();
-        test3();
+        //test1();
+        //test2();
+        //test3();
+        test4();
     }
 
     private static void test1() {
@@ -18,7 +21,6 @@ public class SQLTests {
                 "WHERE product_name = \"" + "Burak" + "\";";
         System.out.println(sqlStatement);
     }
-
     private static void test2() throws SQLException {
         System.out.println(CheckIfRowExist.isProductNameExistInProductTable("Burak"));
         System.out.println(CheckIfRowExist.isProductNameExistInProductTable("Bagietka - BBQ Strips"));
@@ -31,4 +33,8 @@ public class SQLTests {
         }
     }
 
+
+    private static void test4() {
+        SelectFromDaysStatistics.getMacroFromDaysStatisticsByDate("2024-05-30");
+    }
 }
