9912018d6e1636e6379d895f03b813072dbffd49
SubZero <lukasz.jagnow@gmail.com>
2024-08-18 22:02:56 +0200

    Add product: Maślanka_Konecka_-_truskawkowa_1000

---
+++ src/runners_and_tests/tests/charts_lib_test/ChartsLibTest.java
@@ -0,0 +1,14 @@
+package runners_and_tests.tests.charts_lib_test;
+
+public class ChartsLibTest {
+    public static void main(String[] args) {
+        /*
+        JFreeChart chart = ChartFactory.createLineChart(
+                "Monthly Sales",
+                "Month",
+                "Sales",
+                dataset);
+
+         */
+    }
+}
