59863c3376d7564fc065d9c8f0ebb18ebc71d736
SubZero <lukasz.jagnow@gmail.com>
2024-09-29 18:00:14 +0200

    Add days: 23.09, 24.09, 25.09, 26.09 and products

--- src/tools/text_files_tools/FilesTools.java
+++ src/tools/text_files_tools/FilesTools.java
@@ -405,7 +405,7 @@ public class FilesTools {
     public static String[] getFullAPathToAllTextFilesInDirectory(String pathToDirectory) {
         // It may cause error: hard code length to 100
 
-        String[] fullPathToFiles = new String[1500];
+        String[] fullPathToFiles = new String[2000];
         File dir = new File(pathToDirectory);
         File[] directoryListing = dir.listFiles();
         int counter = 0;
