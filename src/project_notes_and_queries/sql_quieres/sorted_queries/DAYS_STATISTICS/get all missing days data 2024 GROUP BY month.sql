e3b61c71507880b245b7933dfc48cd45c44554ef
SubZero <lukasz.jagnow@gmail.com>
2025-01-24 12:42:34 +0100

    Add days 19.1, 20.1, 21.1, 22.1, 23.1 -> +5 and products

---
+++ src/project_notes_and_queries/sql_quieres/sorted_queries/DAYS_STATISTICS/get all missing days data 2024 GROUP BY month.sql
@@ -0,0 +1,7 @@
+SELECT Count(*), SUBSTRING(day_date, 1, 7) FROM days_statistics_test 
+WHERE amount_of_points_from_notepad != amount_of_filled_points_from_notepad 
+AND
+amount_of_filled_points_from_notepad = 0
+AND
+day_date LIKE "2024-%"
+GROUP BY SUBSTRING(day_date, 1, 7); 
