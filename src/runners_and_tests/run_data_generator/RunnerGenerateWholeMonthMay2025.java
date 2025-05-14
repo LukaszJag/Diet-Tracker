be7e2484ac03a287c02a8be3192b411b66de31c6
SubZero <lukasz.jagnow@gmail.com>
2025-05-14 12:37:18 +0200

    Add generate, update, SQL files - May - 2025

---
+++ src/runners_and_tests/run_data_generator/RunnerGenerateWholeMonthMay2025.java
@@ -0,0 +1,10 @@
+package runners_and_tests.run_data_generator;
+
+import tools.sql_tools.days_statistics.GenerateSLQTableForDaysStatistics;
+
+public class RunnerGenerateWholeMonthMay2025 {
+    public static void main(String[] args) {
+        GenerateSLQTableForDaysStatistics.generateWholeMonth("May", 2025);
+        GenerateSLQTableForDaysStatistics.generateWholeMonthAndFillAmountOfPointsFromNotepad("May", 2025);
+    }
+}
