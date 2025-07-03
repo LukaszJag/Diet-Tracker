89aadd1a2870bb7438e836b317f474f2fdf25b04
SubZero <lukasz.jagnow@gmail.com>
2025-07-03 11:01:52 +0300

    Start - Create and setup SQL table for workout #52

--- src/data_store_and_backup/sql_query/CREATE GYM CALENDAR.sql
+++ src/data_store_and_backup/sql_query/CREATE GYM CALENDAR.sql
@@ -31,3 +31,48 @@ CREATE TABLE `gym_calendar` (
   `comment_optional` varchar(160) DEFAULT NULL,
   PRIMARY KEY (`product_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+
+CREATE TABLE `table_for_workout` (
+   `ID` int DEFAULT NULL
+  `day_date` date NOT NULL,
+  `day_name` varchar(45) NOT NULL,
+   `type_of_workout` varchar(100) DEFAULT NULL,
+   `location_of_workout` varchar(100) DEFAULT NULL,
+  `enter_workout_area_full_time_date` date DEFAULT NULL,
+  `start_workout_time_full` datetime DEFAULT NULL,
+   `end_workout_time_full` datetime DEFAULT NULL
+   `duration_workout_time_seconds` int DEFAULT NULL
+   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+  `product_name` varchar(80) DEFAULT NULL,
+  `amount_of_product` float DEFAULT NULL,
+  `kcal` float DEFAULT NULL,
+  `protein` float DEFAULT NULL,
+  `fat` float DEFAULT NULL,
+  `carbs` float DEFAULT NULL,
+  `time_optional` datetime DEFAULT NULL,
+  `comment_optional` varchar(80) DEFAULT NULL,
+  `kcal_consume` float DEFAULT NULL,
+  `carbs_consume` float DEFAULT NULL,
+  `fat_consume` float DEFAULT NULL,
+  `protein_consume` float DEFAULT NULL,
+  `meal_name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+Must be fileds:
+Date
+Type of trening
+Location
+
+Optionals fields:
+ID
+Start time
+End Time
+Duration
+
---
+++ src/project_notes_and_queries/sql_quieres/sorted_queries/TABLE FOR WORKOUT/CREATE TABLE table_for_workout.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `table_for_workout` (
+   `ID` int DEFAULT NULL,
+  `day_date` date NOT NULL,
+  `day_name` varchar(45) NOT NULL,
+   `type_of_workout` varchar(100) DEFAULT NULL,
+   `location_of_workout` varchar(100) DEFAULT NULL,
+  `enter_workout_area_full_time_date` date DEFAULT NULL,
+  `start_workout_time_full` datetime DEFAULT NULL,
+   `end_workout_time_full` datetime DEFAULT NULL,
+   `duration_workout_time_seconds` int DEFAULT NULL
+   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
