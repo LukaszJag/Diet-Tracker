94bb9a84a482f0c14e3622adfd497878e3ba6de8
SubZero <lukasz.jagnow@gmail.com>
2025-08-11 23:30:13 +0200

    Continue - making structure for future input windows GUI

--- src/gui/general/SimpleInputWindowGUI.java
+++ src/gui/general/SimpleInputWindowGUI.java
@@ -4,14 +4,15 @@ import configuration.Config;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public abstract class SimpleInputWindowGUI {
     //<editor-fold desc="Panels">
-    JPanel panelMain = new JPanel();
-    JPanel panelNorth = new JPanel();
-    JPanel panelWest = new JPanel();
-    JPanel panelEast = new JPanel();
-    JPanel panelSouth = new JPanel();
+    JPanel mainPanel = new JPanel();
+    JPanel northPanel = new JPanel();
+    JPanel westPanel = new JPanel();
+    JPanel eastPanel = new JPanel();
+    JPanel southPanel = new JPanel();
     //</editor-fold>
 
     JFrame windowFrame;
@@ -28,21 +29,49 @@ public abstract class SimpleInputWindowGUI {
     }
 
     private void startAddNewProductWindow() {
-        windowFrame = new JFrame(windowName);
-        windowFrame.setSize(windowSize);
+        setFrame();
         setPanels();
         addPanelsToFrame();
         setComponents();
         addComponentsToPanels();
+    }
 
+    private void setFrame() {
+        windowFrame = new JFrame(windowName);
+        windowFrame.setSize(windowSize);
         windowFrame.setLocationRelativeTo(null);
         windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         windowFrame.setResizable(false);
         windowFrame.setVisible(true);
     }
 
+    //<editor-fold desc="addComponentsToPanels">
     private void addComponentsToPanels() {
+        addComponentsToMainPanel(null, null);
+
+        addComponentsToNorthPanel();
+        addComponentsToWestPanel();
+        addComponentsToEastPanel();
+        addComponentsToSouthPanel();
+
     }
+
+    private void addComponentsToMainPanel(ArrayList<JLabel> labelsToAdd, ArrayList<?> mainElements) {
+
+    }
+
+    private void addComponentsToSouthPanel() {
+    }
+
+    private void addComponentsToEastPanel() {
+    }
+
+    private void addComponentsToWestPanel() {
+    }
+
+    private void addComponentsToNorthPanel() {
+    }
+    //</editor-fold>
 
     private void setComponents() {
     }
