3ab8918efd180b5ff0e8c3d136a4e854f180a42f
SubZero <lukasz.jagnow@gmail.com>
2025-08-21 14:20:04 +0200

    Start making a different approach to create multiple input windows

---
+++ src/gui/general/InputPatternWindowGUI.java
@@ -0,0 +1,147 @@
+package gui.general;
+
+import configuration.Config;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class InputPatternWindowGUI {
+    //<editor-fold desc="Panels">
+    JPanel mainPanel = new JPanel();
+    JPanel northPanel = new JPanel();
+    JPanel westPanel = new JPanel();
+    JPanel eastPanel = new JPanel();
+    JPanel southPanel = new JPanel();
+    //</editor-fold>
+
+    JFrame windowFrame;
+
+    String windowName;
+
+    int windowWidth;
+    int windowHeight;
+
+    Dimension windowSize = Config.SIMPLE_INPUT_WINDOW_GUI_WINDOW_SIZE;
+
+    GridLayout gridLayoutMainPanel = new GridLayout(17, 3, 0, 0);
+
+    ArrayList<String> filedNamesData = new ArrayList<>();
+    ArrayList<JTextField> textFieldToHoldData = new ArrayList<>();
+
+    public JButton acceptButton = new JButton("Accept");
+
+
+    public InputPatternWindowGUI(){
+        setupEmptyInstant();
+    }
+
+    private void setupEmptyInstant() {
+        setFrame();
+        setPanels();
+        addPanelsToFrame();
+        setGeneralComponents();
+    }
+
+    public void displayWindow(){
+        windowFrame.setLocationRelativeTo(null);
+        windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windowFrame.setResizable(false);
+        windowFrame.setVisible(true);
+    }
+
+    public void startAddNewProductWindow() {
+        setFrame();
+        setPanels();
+        addPanelsToFrame();
+        windowFrame.setLocationRelativeTo(null);
+        windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windowFrame.setResizable(false);
+        windowFrame.setVisible(true);
+    }
+
+    public void setFrame() {
+        windowFrame = new JFrame(windowName);
+        windowFrame.setSize(windowSize);
+
+    }
+
+    public void setPanels() {
+        // mainPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        mainPanel.setLayout(gridLayoutMainPanel);
+        westPanel.setBackground(Color.BLACK);
+        eastPanel.setBackground(Color.GRAY);
+        mainPanel.setBackground(Color.DARK_GRAY);
+        northPanel.setBackground(Color.BLUE);
+        southPanel.setBackground(Color.WHITE);
+
+        westPanel.setPreferredSize(new Dimension(150, 40));
+        eastPanel.setPreferredSize(new Dimension(200, 100));
+        mainPanel.setPreferredSize(new Dimension(50, 100));
+        northPanel.setPreferredSize(new Dimension(100, 50));
+        southPanel.setPreferredSize(new Dimension(100, 80));
+    }
+
+
+
+    public ArrayList<String> getDataFromEveryTextField(){
+        ArrayList<String> dataToReturn = new ArrayList<String>();
+
+        for (int i = 0; i < textFieldToHoldData.size(); i++) {
+            dataToReturn.add(textFieldToHoldData.get(i).getText());
+        }
+        return null;
+    }
+
+    public void addComponentsToMainPanel(ArrayList<String> fieldsToAdd) {
+
+
+        for (int i = 0; i < fieldsToAdd.size(); i++) {
+            mainPanel.add(new JLabel(fieldsToAdd.get(i))).setForeground(Color.BLACK);
+            String textForTextField = "" + i;
+            textFieldToHoldData.add(i, new JTextField());
+            mainPanel.add(textFieldToHoldData.get(i));
+
+        }
+    }
+
+    //<editor-fold desc="addComponentsToPanels">
+    public void addComponentsToSouthPanel() {
+    }
+
+    public void addComponentsToEastPanel() {
+    }
+
+    public void addComponentsToWestPanel() {
+    }
+
+    public void addComponentsToNorthPanel() {
+    }
+    //</editor-fold>
+
+    public void addPanelsToFrame() {
+        windowFrame.add(westPanel, BorderLayout.WEST);
+        windowFrame.add(eastPanel, BorderLayout.EAST);
+        windowFrame.add(mainPanel, BorderLayout.CENTER);
+        windowFrame.add(northPanel, BorderLayout.NORTH);
+        windowFrame.add(southPanel, BorderLayout.SOUTH);
+    }
+
+    public void setGeneralComponents(){
+        acceptButton.addActionListener(new AcceptButtonActionListener());
+        southPanel.add(acceptButton);
+    }
+
+    public ActionListener getAcceptButtonActionListener(){
+        return acceptButton.getActionListeners()[0];
+    }
+
+    private class AcceptButtonActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+}
---
+++ src/gui/gym/PlayOnFieldInputGUI.java
@@ -0,0 +1,26 @@
+package gui.gym;
+
+import gui.general.InputPatternWindowGUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PlayOnFieldInputGUI {
+    public static void main(String[] args) {
+        InputPatternWindowGUI inputPatternWindowGUI = new InputPatternWindowGUI();
+
+        ArrayList<String> fieldsToAdd = new ArrayList<String>(Arrays.asList("workout_ID", "day_date", "day_name", "location_of_workout",
+                "general_type_of_workout", "type_of_workout", "name_of_exercise", "number_of_set",
+                "reps", "duration_of_break_seconds",
+                "duration_in_seconds", "distance_in_meters", "amount_of_sets", "comment"));
+        inputPatternWindowGUI.addComponentsToMainPanel(fieldsToAdd);
+
+        inputPatternWindowGUI.displayWindow();
+        ActionListener acceptButtonActionListener = inputPatternWindowGUI.getAcceptButtonActionListener();
+
+        ActionEvent actionEvent = null;
+        acceptButtonActionListener.actionPerformed(actionEvent);
+    }
+}
