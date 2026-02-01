2351fb241a376cb6af37061d72a6c7744d799a7a
SubZero <lukasz.jagnow@gmail.com>
2026-02-01 16:53:12 +0100

    Done - #107

--- src/gui/diet/AddProductToCalendarDay.java
+++ src/gui/diet/AddProductToCalendarDay.java
@@ -1522,7 +1522,7 @@ public class AddProductToCalendarDay {
             // TODO hard coded value "2026-01%" - need to set general value
             LinkedHashMap<String, String> amountOfProductInSQLTableLinkedHashMap = Select.selectAllDataFromTable(
                     "days_statistics_test", "day_date", "amount_of_filled_points_from_notepad", "day_date",
-                    "LIKE", "2026-01%");
+                    "LIKE", "2026-02%");
 
             String pointInOneDay = "";
 
