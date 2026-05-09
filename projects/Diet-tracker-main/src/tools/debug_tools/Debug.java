6983588135f4477018290eac6f09a25973d4f53c
SubZero <23236971+LukaszJag@users.noreply.github.com>
2026-05-09 10:31:48 +0200

    Add - printKeyAndValue - method - Debug.java

--- projects/Diet-tracker-main/src/tools/debug_tools/Debug.java
+++ projects/Diet-tracker-main/src/tools/debug_tools/Debug.java
@@ -14,4 +14,15 @@ public class Debug {
     public static void printOrangeSystemPrintln(String nameOfMethod, String variableName, String value){
         System.out.println(ANSI_YELLOW + nameOfMethod + " \t->\t " + variableName + " \t->\t " + value +ANSI_RESET);;
     }
+
+    public static void printKeyAndValue(String key, String value, int amountOfTabsBetween){
+        int amountOfTabs = (key.length()+1) / 4;
+        String tabs = "";
+
+        for (int j = 0; j < 6-amountOfTabs; j++) {
+            tabs += "\t";
+        }
+
+        System.out.println("key: " + key + tabs + " value: " + value);
+    }
 }
