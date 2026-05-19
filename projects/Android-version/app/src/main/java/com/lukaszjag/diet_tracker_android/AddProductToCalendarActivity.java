cb15bb91acdbd46e3ecdda4aa224166420283112
SubZero <23236971+LukaszJag@users.noreply.github.com>
2026-05-19 13:47:13 +0200

    Add Data
    Add days 18.5(part of 19.5) -> +1
    Add products -> +1
    Add data to - quick_fill_amount_of_point_in_notepad -> +1

---
+++ projects/Android-version/app/src/main/java/com/lukaszjag/diet_tracker_android/AddProductToCalendarActivity.java
@@ -0,0 +1,13 @@
+package com.lukaszjag.diet_tracker_android;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddProductToCalendarActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_product_to_calendar);
+    }
+}
