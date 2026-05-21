b457e0e6a4f9dde1d97b8cfd211b8b58b40f7a99
SubZero <23236971+LukaszJag@users.noreply.github.com>
2026-05-21 17:29:41 +0200

    Add and setup to code - all TextView and EditText - from add_product_to_calendar.xml

--- projects/Android-version/app/src/main/java/com/lukaszjag/diet_tracker_android/MainActivity.java
+++ projects/Android-version/app/src/main/java/com/lukaszjag/diet_tracker_android/MainActivity.java
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -17,24 +18,106 @@ import com.lukaszjag.diet_tracker_android.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // UI components
+    Button passDataButton;
+
+    TextView tvDate;
+    TextView tvMealName;
+    TextView tvProductName;
+    TextView tvProductSuggestion;
+    TextView tvBrand;
+    TextView tvAmount;
+    TextView tvKcal;
+    TextView tvProtein;
+
+    EditText etDate;
+    EditText etMealName;
+    EditText etProductName;
+    EditText etProductSuggestion;
+    EditText etBrand;
+    EditText etAmount;
+    EditText etKcal;
+    EditText etProtein;
+
+    public void setupComponents(){
+        passDataButton = (Button) findViewById(R.id.passDataButton);
+
+        tvDate = (TextView) findViewById(R.id.date);
+        tvMealName = (TextView) findViewById(R.id.mealName);
+        tvProductName = (TextView) findViewById(R.id.productName);
+        tvProductSuggestion = (TextView) findViewById(
+                R.id.productNameSuggestion
+        );
+        tvBrand = (TextView) findViewById(R.id.brand);
+        tvAmount = (TextView) findViewById(R.id.amountOfProduct);
+        tvKcal = (TextView) findViewById(R.id.kcal);
+        tvProtein = (TextView) findViewById(R.id.protein);
+
+
+        etDate = (EditText) findViewById(R.id.dateEditText);
+        etMealName = (EditText) findViewById(R.id.mealNameEditText);
+        etProductName = (EditText) findViewById(R.id.productNameEditText);
+        etProductSuggestion = (EditText) findViewById(R.id.productNameSuggestionEditText);
+        etBrand = (EditText) findViewById(R.id.brandEditText);
+        etAmount = (EditText) findViewById(R.id.amountOfProductEditText);
+        etKcal = (EditText) findViewById(R.id.kcalEditText);
+        etProtein = (EditText) findViewById(R.id.proteinEditText);
+    }
+
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //setupComponents();
+
+        setContentView(R.layout.add_product_to_calendar);
+
+        Button passDataButton;
+
+        EditText etDate;
+        EditText etMealName;
+        EditText etProductName;
+        EditText etProductSuggestion;
+        EditText etBrand;
+        EditText etAmount;
+        EditText etKcal;
+        EditText etProtein;
+
+        passDataButton = (Button) findViewById(R.id.passDataButton);
+
+        etDate = (EditText) findViewById(R.id.dateEditText);
+        etMealName = (EditText) findViewById(R.id.mealNameEditText);
+        etProductName = (EditText) findViewById(R.id.productNameEditText);
+        etProductSuggestion = (EditText) findViewById(R.id.productNameSuggestionEditText);
+        etBrand = (EditText) findViewById(R.id.brandEditText);
+        etAmount = (EditText) findViewById(R.id.amountOfProductEditText);
+        etKcal = (EditText) findViewById(R.id.kcalEditText);
+        etProtein = (EditText) findViewById(R.id.proteinEditText);
+
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.add_product_to_calendar);
 
-        //setSupportActionBar(binding.toolbar);
-
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        passDataButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("data", String.valueOf(etDate.getEditableText()));
+                Log.i("data", String.valueOf(etMealName.getEditableText()));
+                Log.i("data", String.valueOf(etProductName.getEditableText()));
+                Log.i("data", String.valueOf(etProductSuggestion.getText()));
+                Log.i("data", String.valueOf(etBrand.getText()));
+                Log.i("data", String.valueOf(etAmount.getText()));
+                Log.i("data", String.valueOf(etKcal.getText()));
+                Log.i("data", String.valueOf(etProtein.getText()));
+            }
+        });
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
